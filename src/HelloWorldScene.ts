7bd538502e38aee9c6b5db47dc3f60836b18f0ce
maritocuate <mario300@gmail.com>
2024-03-16 13:50:54 -0300

    ✨ add character

--- src/HelloWorldScene.ts
+++ src/HelloWorldScene.ts
@@ -1,15 +1,83 @@
 import { Scene } from 'phaser'
 
 export default class HelloWorldScene extends Scene {
+  private player?: Phaser.Physics.Arcade.Sprite
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
+
   constructor() {
     super('hello-world')
   }
 
   preload() {
-    this.load.image('sky', 'assets/sky.png')
+    this.load.image('map', 'assets/earthbound-scarab.png')
+
+    this.load.spritesheet('dude', 'assets/marito.png', {
+      frameWidth: 64,
+      frameHeight: 64,
+    })
   }
   create() {
-    this.add.image(400, 300, 'sky')
+    this.cameras.main.setBounds(0, 0, 800, 600)
+
+    this.add.image(0, 0, 'map').setOrigin(0).setScrollFactor(1)
+
+    this.player = this.physics.add.sprite(400, 400, 'dude')
+    this.player.setBounce(0.2)
+    this.player.setCollideWorldBounds(true)
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', {
+        frames: [9, 10, 11, 12, 13, 14, 15, 16],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 19 }],
+      frameRate: 20,
+    })
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 0,
+        end: 7,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      key: 'top',
+      frames: this.anims.generateFrameNumbers('dude', {
+        frames: [28, 29, 30, 31, 32, 33, 34, 35],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
-  update() {}
+  update() {
+    if (!this.cursors) {
+      return
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player?.setVelocityX(-160)
+      this.player?.anims.play('left', true)
+    } else if (this.cursors.right.isDown) {
+      this.player?.setVelocityX(160)
+      this.player?.anims.play('right', true)
+    } else if (this.cursors.up.isDown) {
+      this.player?.setVelocityY(-160)
+      this.player?.anims.play('top', true)
+    } else {
+      this.player?.setVelocityX(0)
+      this.player?.anims.play('turn')
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
-import Phaser from "phaser";
+import Phaser from 'phaser'
 
-import HelloWorldScene from "./HelloWorldScene";
+import HelloWorldScene from './HelloWorldScene'
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  parent: "app",
-  width: 800,
-  height: 600,
+  parent: 'app',
+  width: 400,
+  height: 300,
+  pixelArt: true,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-      gravity: { y: 200 },
+      debug: true,
     },
   },
   scene: [HelloWorldScene],
-};
+}
 
-export default new Phaser.Game(config);
+export default new Phaser.Game(config)
