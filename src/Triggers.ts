2c6afd6692acac39b99ebb4b89ab01be47ab705d
maritocuate <mario300@gmail.com>
2024-04-19 19:22:08 -0300

    ✨ contact popup update

--- src/Triggers.ts
+++ src/Triggers.ts
@@ -13,7 +13,7 @@ export function createTriggers(physics: Physics.Arcade.ArcadePhysics) {
   const contact = triggers.create(225, 420, 'obstacle', undefined, false)
   contact.setSize(10, 3)
   contact.text =
-    'You can send me\nan email here\n mario300@gmail.com\nbefore the\nmail man comes by.'
+    'Write me here \n mario300@gmail.com\nbefore the\nmail man comes by.'
 
   return triggers
 }
